fa30cd1d528c86bebc1369d906a8c557eac41112
yogesh konduru <yogeshkonduru@gmail.com>
2022-10-28 08:31:00 +0530

    small config changes

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0" # update as per your need
 
-REPO_NAME = "<YOUR REPO NAME>"
-AUTHOR_USER_NAME = "<YOUR USERNAME>"
-SRC_REPO = "<YOUR REPO NAME>"
-AUTHOR_EMAIL = "<YOUR EMAIL ID>"
+REPO_NAME = "example_pypi"
+AUTHOR_USER_NAME = "yogeshkonduru"
+SRC_REPO = "example_pypi"
+AUTHOR_EMAIL = "yogeshkonduru@gmail.con"
 
 setuptools.setup(
     name=SRC_REPO,
